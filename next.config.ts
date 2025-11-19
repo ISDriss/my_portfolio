74c89f3dddec2abaf595a1c7e9d4e6da8a674f3e
Ilian <ilian.sebti@gmail.com>
2025-11-19 19:06:28 +0100

    image fix for GH actions + README

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from 'next';
 
+const repoName = 'my_portfolio';
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: isGithubActions ? `/${repoName}` : undefined,
+  assetPrefix: isGithubActions ? `/${repoName}/` : undefined,
 };
 
 export default nextConfig;
